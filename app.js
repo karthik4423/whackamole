3a8af2b781b96b39efed5ef473c840d0ee6654be
Karthik Raj <karthikraj4423@gmail.com>
2020-04-11 01:20:47 +0530

    added score in cheer

--- app.js
+++ app.js
@@ -31,32 +31,32 @@ square.forEach(id => {
             score.textContent = result
         }
         if (result > 10 && first10) {
-            cheer.textContent = "Whooho!"
+            cheer.textContent = "10 Points! Whooho!"
             first10 = false
         }
         if (result > 25 && first25) {
             timeout = timeout - 150
-            cheer.textContent = "You're On a Roll!"
+            cheer.textContent = "25! You're On a Roll!"
             first25 = false
         }
         if (result > 35 && first35) {
             timeout = timeout - 100
-            cheer.textContent = "LEGENDARY!"
+            cheer.textContent = "35! LEGENDARY!"
             first35 = false
         }
         if (result > 50 && first50) {
             timeout = timeout - 150
-            cheer.textContent = "How much can you score?!"
+            cheer.textContent = "50!! How much can you score?!"
             first50 = false
         }
         if (result > 65 && first65) {
             timeout = timeout - 150
-            cheer.textContent = "Whack-a-mole GURU!"
+            cheer.textContent = "65! Whack-a-mole GURU!"
             first65 = false
         }
         if (result > 75 && first75) {
             timeout = timeout - 50
-            cheer.textContent = "Whack-a-BOSS!"
+            cheer.textContent = "75! Whack-a-BOSS!"
             first75 = false
         }
     })
