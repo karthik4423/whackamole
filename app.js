ee6e4fc2f962e56be29ad9495a24d4657b96c141
Karthik Raj <karthikraj4423@gmail.com>
2020-04-11 01:55:01 +0530

    added variable speed for mole depending on score

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let cheer = document.querySelector('#cheer')
 let result = 0
 let currentTime = timeleft.textContent
 
-var timeout = 1000
+timeout = 1000
 
 alert("This is the classic Whack-a-mole Game.\n Click okay to begin.")
 
@@ -22,7 +22,6 @@ function randomSquare() {
         //assign the id of randomPosition to hitPosition for us to see later
     hitPosition = randomPosition.id
 }
-var timeout = 1000
 var first10 = first25 = first35 = first50 = first65 = first75 = true
 square.forEach(id => {
     id.addEventListener('mouseup', () => {
@@ -31,40 +30,54 @@ square.forEach(id => {
             score.textContent = result
         }
         if (result > 10 && first10) {
-            cheer.textContent = "10 Points! Whooho!"
+            cheer.textContent = "10 Points! Whooho!\nSpeeding UP!"
             first10 = false
+            timeout = timeout - 200
+                //moveMole(800)
         }
         if (result > 25 && first25) {
-            timeout = timeout - 150
-            cheer.textContent = "25! You're On a Roll!"
             first25 = false
+            timeout = timout - 150
+            cheer.textContent = "25! You're On a Roll!"
+
         }
         if (result > 35 && first35) {
+            first35 = false
             timeout = timeout - 100
             cheer.textContent = "35! LEGENDARY!"
-            first35 = false
+
         }
         if (result > 50 && first50) {
-            timeout = timeout - 150
-            cheer.textContent = "50!! How much can you score?!"
             first50 = false
+            timeout = timeout - 100
+            cheer.textContent = "50!! How much can you score?!"
+
         }
         if (result > 65 && first65) {
-            timeout = timeout - 150
-            cheer.textContent = "65! Whack-a-mole GURU!"
             first65 = false
+            timeout = timeout - 50
+            cheer.textContent = "65! Whack-a-mole GURU!"
+
         }
         if (result > 75 && first75) {
-            timeout = timeout - 50
+            //timeout = timeout - 50
             cheer.textContent = "75! Whack-a-BOSS!"
             first75 = false
         }
     })
 })
 
-function moveMole() {
+function randomtick() {
+    setTimeout(() => {
+        moveMole()
+        randomtick()
+    }, timeout)
+}
+
+function moveMole(timeout = 1000) {
     let timerId = null
-    timerId = setInterval(randomSquare, timeout)
+    randomSquare()
+        //timerId = setInterval(randomSquare, timeout)
     console.log(timeout)
 }
 
@@ -78,6 +91,6 @@ function countDown() {
         location = location
     }
 }
-
-moveMole()
+randomtick()
+    //moveMole()
 let timerId = setInterval(countDown, 1000)
